3137a7ea08c656028504353ea6cb69d8e4abebfc
sena <gulozseven@gmail.com>
2025-07-20 19:03:05 +0300

    Employee.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,20 @@
 package org.example;
-
+import org.example.entity.Employee;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        List<Employee> employees = new LinkedList<>();
+        Employee employee1 = new Employee(1, "ucubik", "fırıldak");
+        Employee employee2 = new Employee(1, "ucubik", "fırıldak");
+        Employee employee3 = new Employee(3, "ucubik", "fırıldak");
+        Employee employee4 = new Employee(1, "zottiri", "fırıldak");
+        Employee employee5 = new Employee(4, "bambaska", "bir sey");
+
+        List<String> findDuplicates() {
+
+        }
     }
 }
---
+++ src/main/java/org/example/entity/Employee.java
@@ -0,0 +1,49 @@
+package org.example.entity;
+
+
+import java.util.Objects;
+
+public class Employee {
+    private long id;
+    private String firstname;
+    private String lastname;
+
+    public Employee(long id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return getId() == employee.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+}
